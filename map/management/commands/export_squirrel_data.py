de12f594b62bb1d3b604fb87715fd055acca2287
xialaoshu30 <54729829+xialaoshu30@users.noreply.github.com>
2021-04-08 12:09:15 +0800

    Update export_squirrel_data.py

--- map/management/commands/export_squirrel_data.py
+++ map/management/commands/export_squirrel_data.py
@@ -1,1 +1,19 @@
 #export
+from django.core.management import BaseCommand
+from map.models import Squirrel
+import csv
+import sys
+
+class Command(BaseCommand):
+	
+	def add_arguements(self, parser):
+		parser.add_arguement('csv_file')
+
+	def handle(self, path, **kwargs):
+		with open(path, 'w', newline = '') as f:
+			model = Squirrel
+			field_names = [f.name for f in model._meta.fields]
+			writer = csv.writer(f, quoting = csv.QUOTE_ALL)
+			writer.writerow(field_names)
+			for j in model.objects.all():
+				writer.writerow([getattr(i, f) for f in field_names])
