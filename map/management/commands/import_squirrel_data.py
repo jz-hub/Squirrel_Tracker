1a8b8030fe6f75d3d5ae013290c2cea0fcd41f9c
xialaoshu30 <54729829+xialaoshu30@users.noreply.github.com>
2021-04-08 18:22:05 +0800

    Update import_squirrel_data.py

--- map/management/commands/import_squirrel_data.py
+++ map/management/commands/import_squirrel_data.py
@@ -16,30 +16,30 @@ class Command(BaseCommand):
             reader = csv.DictReader(f)
             data = list(reader)
         for i in data:
-			sq = Squirrel(
-				X = i['X'],
-				Y = i['Y'],
-				shift = i['Shift'],
-				date = i['Date'],
-				unique_squirrel_id = i['Unique_squirrel_id'],
-				age = i['Age'],
-				primary_fur_color = i['Primary_fur_color'],
-				location = i['Lacation'],
-				specific_location = i['Specific_location'],
-				running = strtobool(i['Running']),
-				chasing = strtobool(i['Chasing']),
-				climbing = strtobool(i['Climbing']),
-				eating = strtobool(i['Eating']),
-				foraging = strtobool(i['Foraging']),
-				other_activities = strtobool(i['Other Activities']),
-				kuks = strtobool(i['Kuks']),
-				quaas = strtobool(i['Quaas']),
-				moans = strtobool(i['Moans']),
-				tail_flags = strtobool(i['Tail flags']),
-				tail_twitching = strtobool(i['Tail twitches']),
-				approaches = strtobool(i['Approaches']),
-				indifferent = strtobool(i['Indifferent']),
-				runs_from = strtobool(i['Runs from']),
-			)
+            sq = Squirrel(
+                X = i['X'],
+                Y = i['Y'],
+                shift = i['Shift'],
+                date = i['Date'],
+                unique_squirrel_id = i['Unique_squirrel_id'],
+                age = i['Age'],
+                primary_fur_color = i['Primary_fur_color'],
+                location = i['Lacation'],
+                specific_location = i['Specific_location'],
+                running = strtobool(i['Running']),
+                chasing = strtobool(i['Chasing']),
+                climbing = strtobool(i['Climbing']),
+                eating = strtobool(i['Eating']),
+                foraging = strtobool(i['Foraging']),
+                other_activities = strtobool(i['Other Activities']),
+                kuks = strtobool(i['Kuks']),
+                quaas = strtobool(i['Quaas']),
+                moans = strtobool(i['Moans']),
+                tail_flags = strtobool(i['Tail flags']),
+                tail_twitching = strtobool(i['Tail twitches']),
+                approaches = strtobool(i['Approaches']),
+                indifferent = strtobool(i['Indifferent']),
+                runs_from = strtobool(i['Runs from']),
+            )
 
-			sq.save()
+            sq.save()
