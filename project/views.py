468b17c5211a980ce526b8193d85c1dda7f4103d
Wen Zhang <wz2519@columbia.edu>
2021-04-09 09:08:46 +0000

    add home page

--- project/views.py
+++ project/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render 
 
+
 def index(request):
-    return render(request, 'project/index.html')
+    return render(request, 'map/base.html')
 
 
 
